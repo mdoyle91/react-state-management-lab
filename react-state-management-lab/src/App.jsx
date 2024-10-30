f70d3505913c429b69d8382c7d27d041c431ea87
mdoyle91 <mike.w.doyle91@gmail.com>
2024-10-30 17:09:49 -0400

    Steps 1 & 2 Completed

--- react-state-management-lab/src/App.jsx
+++ react-state-management-lab/src/App.jsx
@@ -1,7 +1,100 @@
 // src/App.jsx
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [zombieFighters, setZombieFighters] = useState([
+    {
+      name: "Survivor",
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: "https://via.placeholder.com/150/92c952",
+    },
+    {
+      name: "Scavenger",
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: "https://via.placeholder.com/150/771796",
+    },
+    {
+      name: "Shadow",
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: "https://via.placeholder.com/150/24f355",
+    },
+    {
+      name: "Tracker",
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: "https://via.placeholder.com/150/d32776",
+    },
+    {
+      name: "Sharpshooter",
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: "https://via.placeholder.com/150/1ee8a4",
+    },
+    {
+      name: "Medic",
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: "https://via.placeholder.com/150/66b7d2",
+    },
+    {
+      name: "Engineer",
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: "https://via.placeholder.com/150/56acb2",
+    },
+    {
+      name: "Brawler",
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: "https://via.placeholder.com/150/8985dc",
+    },
+    {
+      name: "Infiltrator",
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: "https://via.placeholder.com/150/392537",
+    },
+    {
+      name: "Leader",
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: "https://via.placeholder.com/150/602b9e",
+    },
+  ]);
+
+  return (
+    <>
+      <div>
+        {zombieFighters.map((zombieFighter, idx) => (
+          <ul key={idx}>
+            <li> Name: {zombieFighter.name} </li>
+            <li> Price: ${zombieFighter.price} </li>
+            <li> Strength: {zombieFighter.strength} </li>
+            <li> Agility: {zombieFighter.agility} </li>
+            <img src={zombieFighter.img} />
+            <button>Add</button>
+          </ul>
+        ))}
+      </div>
+      <h1>Money: {money}</h1>
+    </>
+  );
 };
 
 export default App;
